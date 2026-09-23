34aa326c24dce801a29475e3b37dddde60b31371
nicolasmcostantini <costantininicolas2@gmail.com>
2026-09-23 17:20:41 -0300

    feat: add sharp image processing service with encoder options and quality handling

--- dist/server/chunks/node_CT__FVay.mjs
+++ dist/server/chunks/node_CT__FVay.mjs
@@ -1346,7 +1346,7 @@ var cssFitValues = [
 ];
 async function getConfiguredImageService() {
 	if (!globalThis?.astroAsset?.imageService) {
-		const { default: service } = await import("./sharp_Doreev7i.mjs").catch((e) => {
+		const { default: service } = await import("./sharp_QxSe02Le.mjs").catch((e) => {
 			const error = new AstroError(InvalidImageService);
 			error.cause = e;
 			throw error;
@@ -1527,7 +1527,7 @@ var $$Image = createComponent(async ($$result, $$props, $$slots) => {
 		...image.attributes
 	};
 	return renderTemplate`${maybeRenderHead($$result)}<img${addAttribute(image.src, "src")}${spreadAttributes(attributes)}${addAttribute(className, "class")}>`;
-}, "/home/nico/Escritorio/webs/aurova/node_modules/astro/components/Image.astro", void 0);
+}, "/home/nico/Escritorio/webs/aurova_software/node_modules/astro/components/Image.astro", void 0);
 //#endregion
 //#region node_modules/astro/components/Picture.astro
 createAstro("https://astro.build");
@@ -1594,7 +1594,7 @@ var $$Picture = createComponent(async ($$result, $$props, $$slots) => {
 		const srcsetAttribute = props.densities || !props.densities && !props.widths && !useResponsive ? `${image.src}${image.srcSet.values.length > 0 ? ", " + image.srcSet.attribute : ""}` : image.srcSet.attribute;
 		return renderTemplate`<source${addAttribute(srcsetAttribute, "srcset")}${addAttribute(mime.lookup(image.options.format ?? image.src) ?? `image/${image.options.format}`, "type")}${spreadAttributes(sourceAdditionalAttributes)}>`;
 	})}<img${addAttribute(fallbackImage.src, "src")}${spreadAttributes(attributes)}${addAttribute(className, "class")}></picture>`;
-}, "/home/nico/Escritorio/webs/aurova/node_modules/astro/components/Picture.astro", void 0);
+}, "/home/nico/Escritorio/webs/aurova_software/node_modules/astro/components/Picture.astro", void 0);
 //#endregion
 //#region \0virtual:astro:assets/fonts/internal
 var componentDataByCssVariable = /* @__PURE__ */ new Map([]);
@@ -1634,7 +1634,7 @@ var $$Font = createComponent(($$result, $$props, $$slots) => {
 	});
 	const filteredPreloadData = filterPreloads(data.preloads, preload);
 	return renderTemplate`<style>${unescapeHTML(data.css)}</style>${filteredPreloadData?.map(({ url, type }) => renderTemplate`<link rel="preload"${addAttribute(url, "href")} as="font"${addAttribute(`font/${type}`, "type")} crossorigin>`)}`;
-}, "/home/nico/Escritorio/webs/aurova/node_modules/astro/components/Font.astro", void 0);
+}, "/home/nico/Escritorio/webs/aurova_software/node_modules/astro/components/Font.astro", void 0);
 //#endregion
 //#region node_modules/astro/dist/assets/fonts/infra/ssr-runtime-font-file-url-resolver.js
 var SsrRuntimeFontFileUrlResolver = class {
@@ -1681,8 +1681,8 @@ Object.defineProperty(imageConfig, "assetQueryParams", {
 var inferRemoteSize = async (url) => {
 	return (await getConfiguredImageService()).getRemoteSize?.(url, imageConfig, _runtimeLogger) ?? inferRemoteSize$1(url, imageConfig);
 };
-var outDir = /* #__PURE__ */ new URL("file:///home/nico/Escritorio/webs/aurova/dist/client/");
-var serverDir = /* #__PURE__ */ new URL("file:///home/nico/Escritorio/webs/aurova/dist/server/");
+var outDir = /* #__PURE__ */ new URL("file:///home/nico/Escritorio/webs/aurova_software/dist/client/");
+var serverDir = /* #__PURE__ */ new URL("file:///home/nico/Escritorio/webs/aurova_software/dist/server/");
 var getImage = async (options) => await getImage$1(options, imageConfig, _runtimeLogger);
 //#endregion
 //#region node_modules/astro/dist/assets/utils/etag.js
--- dist/server/chunks/sharp_QxSe02Le.mjs
+++ dist/server/chunks/sharp_QxSe02Le.mjs
@@ -1,5 +1,5 @@
 import { $ as UnsupportedImageFormat, F as NoImageMetadata, N as MissingSharp, t as AstroError } from "./errors_CAafeVI3.mjs";
-import { a as resolveDefaultOutputFormat, i as detector, n as baseService, r as parseQuality } from "./node_DUGEwZ4Z.mjs";
+import { a as resolveDefaultOutputFormat, i as detector, n as baseService, r as parseQuality } from "./node_CT__FVay.mjs";
 //#region node_modules/astro/dist/assets/services/sharp.js
 var sharp;
 var qualityTable = {
